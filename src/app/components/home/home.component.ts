ef9e6583d1ed52dee102d99a4fd36c7bb98dfe90
matthigg <mhh129@gmail.com>
2020-07-14 21:19:55 -0400

    Add contact section to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: [
+    './home.component.scss',
+    '../our-services/service-components/shared/shared-services-styles.scss',
+  ]
 })
 export class HomeComponent implements OnInit {
 
