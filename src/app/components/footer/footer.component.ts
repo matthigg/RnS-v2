ae9a38e51660d501ac3c96c86da1578e250843c8
matthigg <mhh129@gmail.com>
2020-07-24 19:31:32 -0400

    Add return-to-top smooth scroll functionality

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -14,7 +14,11 @@ export class FooterComponent implements OnInit {
   }
 
   onScrollToTop(): void {
-    
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 
 }
