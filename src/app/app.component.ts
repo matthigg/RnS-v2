457615a246c773d76c68dbc6bdf90caf444fc555
matthigg <mhh129@gmail.com>
2020-08-02 11:27:35 -0400

    Only show nav toolbar buttons if window width > 768px, and nav phone number text if window width > 870px

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 // Angular Material
 import { MatSidenav } from '@angular/material/sidenav';
+
+// RxJS
+import { Subscription } from 'rxjs';
+
+// Models, Services
+import { WindowSizeService } from './services/window-size.service';
+import { WindowSize } from './shared/models/window-size';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'rns-v2';
+export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
   fadeInBackgroundImage: boolean = false;
+  windowSize: WindowSize | null = null;
   @ViewChild('sidenav') sidenav: MatSidenav;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private windowSizeService: WindowSizeService) { }
 
   ngAfterViewInit() {
     setTimeout(_ => { this.fadeInBackgroundImage = true });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.add(this.windowSizeService.windowSize.subscribe(
+      response => this.windowSize = response
+    ));
   }
 
   close(): void {
--- src/app/components/nav-toolbar/nav-toolbar.component.ts
+++ src/app/components/nav-toolbar/nav-toolbar.component.ts
@@ -7,6 +7,8 @@ import { Subscription } from 'rxjs';
 import { BusinessInformationService } from '../../services/business-information.service';
 import { PageOffset } from '../../shared/models/page-offset';
 import { WindowScrollPositionService } from '../../services/window-scroll-position.service';
+import { WindowSizeService } from '../../services/window-size.service';
+import { WindowSize } from '../../shared/models/window-size';
 
 @Component({
   selector: 'app-nav-toolbar',
@@ -16,17 +18,23 @@ import { WindowScrollPositionService } from '../../services/window-scroll-positi
 export class NavToolbarComponent implements OnDestroy, OnInit {
   phoneNumber: string = this.businessInformationService.phoneNumber;
   pageOffset: PageOffset | null = null;
+  windowSize: WindowSize | null = null;
   private subscriptions: Subscription = new Subscription();
 
   constructor(
     private businessInformationService: BusinessInformationService,
-    private windowScrollPositionService: WindowScrollPositionService
+    private windowScrollPositionService: WindowScrollPositionService,
+    private windowSizeService: WindowSizeService
   ) { }
 
   ngOnInit(): void {
     this.subscriptions.add(this.windowScrollPositionService.pageOffset.subscribe(
       response => this.pageOffset = response
     ));
+    this.subscriptions.add(this.windowSizeService.windowSize.subscribe(
+      response => this.windowSize = response
+    ));
+    console.log('--- windowSize:', this.windowSize);
   }
 
   ngOnDestroy(): void {
