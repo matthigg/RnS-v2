0857164c9d3a5a102e711a2466d849dd51b868c7
matthigg <mhh129@gmail.com>
2020-08-05 23:36:34 -0400

    Add content to thank-you component

--- src/app/components/thank-you/thank-you.component.ts
+++ src/app/components/thank-you/thank-you.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { BusinessInformationService } from 'src/app/services/business-information.service';
+
 @Component({
   selector: 'app-thank-you',
   templateUrl: './thank-you.component.html',
   styleUrls: ['./thank-you.component.scss']
 })
 export class ThankYouComponent implements OnInit {
+  phoneNumber: string = this.businessInformationService.phoneNumber;
 
-  constructor() { }
+  constructor(public businessInformationService: BusinessInformationService) { }
 
   ngOnInit(): void {
   }
