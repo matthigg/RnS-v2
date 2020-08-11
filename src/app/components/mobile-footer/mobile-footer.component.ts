5f6cbbc643af6c52c9fee181fa855b01a327d006
matthigg <mhh129@gmail.com>
2020-08-11 18:38:37 -0400

    Set up mobile-footer link & phone number

--- src/app/components/mobile-footer/mobile-footer.component.ts
+++ src/app/components/mobile-footer/mobile-footer.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { BusinessInformationService } from '../../services/business-information.service';
+
 @Component({
   selector: 'app-mobile-footer',
   templateUrl: './mobile-footer.component.html',
   styleUrls: ['./mobile-footer.component.scss']
 })
 export class MobileFooterComponent implements OnInit {
+  phoneNumber: string = this.businessInformationService.phoneNumber;
 
-  constructor() { }
+  constructor(public businessInformationService: BusinessInformationService) { }
 
   ngOnInit(): void {
   }
